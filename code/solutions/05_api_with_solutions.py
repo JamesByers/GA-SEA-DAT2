acc53257f4cb6b3e83f1465db920f280a893dac4
Jim Byers <james.byers.business@gmail.com>
2016-03-28 16:35:33 -0700

    updated L5 code

--- code/solutions/05_api_with_solutions.py
+++ code/solutions/05_api_with_solutions.py
@@ -16,30 +16,28 @@ How to interact with a REST API:
 # read IMDb data into a DataFrame: we want a year column!
 import pandas as pd
 movies = pd.read_csv('../data/imdb_1000.csv')
-
-# Look at top 5 rows
-
+movies.head()
 
 # use requests library to interact with a URL
 import requests
 r = requests.get('http://www.omdbapi.com/?t=the shawshank redemption&r=json&type=movie')
 
 # check the status: 200 means success, 4xx means error
-
+r.status_code
 
 # view the raw response text
-
+r.text
 
 # decode the JSON response body into a dictionary
-
+r.json()
 
 # extracting the year from the dictionary
-
+r.json()['Country']
 
 # what happens if the movie name is not recognized?
 r = requests.get('http://www.omdbapi.com/?t=blahblahblah&r=json&type=movie')
-
-
+r.status_code
+r.json()
 
 # define a function to return the year
 def get_movie_year(title):
@@ -55,7 +53,7 @@ get_movie_year('The Shawshank Redemption')
 get_movie_year('blahblahblah')
 
 # create a smaller DataFrame for testing
-
+top_movies = movies.head().copy()
 
 # write a for loop to build a list of years
 from time import sleep
@@ -65,9 +63,10 @@ for title in top_movies.title:
     sleep(1)
 
 # check that the DataFrame and the list of years are the same length
+assert(len(top_movies) == len(years))
 
 # save that list as a new column
-
+top_movies['year'] = years
 
 '''
 Bonus content: Updating the DataFrame as part of a loop
@@ -83,7 +82,7 @@ for index, row in top_movies.iterrows():
     print index, row.title
 
 # create a new column and set a default value
-
+movies['year'] = -1
 
 # loc method allows you to access a DataFrame element by 'label'
 movies.loc[0, 'year'] = 1994
